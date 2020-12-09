ab89fc622da444e363c4f586b847897eb7bc9b48
ShawnGao <2519306859@qq.com>
2020-12-09 13:42:50 +0800

    Fix a Error

--- C/作业/实验七-结构体-2020/8.c
+++ C/作业/实验七-结构体-2020/8.c
@@ -4,7 +4,7 @@ struct stu_inf {
 	char id[11];
 	char name[9];
 	char college[64];
-	char class[6];
+	char c[6];
 	int age;
 }stu,*point;
 
@@ -17,8 +17,8 @@ int main() {
 	printf("Enter Your College:");
 	gets(stu.college);
 	printf("Enter Your Class:");
-	gets(stu.class);
+	gets(stu.c);
 	printf("Enter Your Age:");
 	scanf("%d",&(point->age));
-	printf("\n\nID:%s\nName:%s\nCollege:%s\nClass:%s\nAge:%d\n",point->id,point->name,point->college,point->class,point->age);
+	printf("\n\nID:%s\nName:%s\nCollege:%s\nClass:%s\nAge:%d\n",point->id,point->name,point->college,point->c,point->age);
 }
